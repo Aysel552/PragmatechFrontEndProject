1e0d577aa6906dc84b1f53b64a9c08e4a73edc47
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-03-18 16:34:38 +0400

    created Task6 file

--- JsFirstSteps/script/Task3.js
+++ JsFirstSteps/script/Task3.js
@@ -2,9 +2,9 @@
 
 
 function calc(x){
-    console.log(`${parseFloat(x%10)}`)
+    console.log(` ${parseInt(x/10)} ${x%10}`)
 }
-calc( 4.234)
+calc(57);
 
 
 
---
+++ JsFirstSteps/script/Task6.js
@@ -0,0 +1,3 @@
+// 3 nöqtənin koordinatları verilir.Bu verilən koordinatlara görə a nöqtəsinə ən yaxın nöqtəni tapan proqram yazın
+
+
