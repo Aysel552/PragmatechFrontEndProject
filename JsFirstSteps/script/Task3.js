22b647ba84f671cd8356c6e0c35ebcce54db48e3
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-03-25 20:58:42 +0400

    Task3 is corrected

--- JsFirstSteps/script/Task3.js
+++ JsFirstSteps/script/Task3.js
@@ -1,10 +1,9 @@
 // verilən x ədədinin rəqəmlərini ekrana çap edən proqram yazın
 
-
-function calc(x){
-    console.log(` ${parseInt(x/10)} ${x%10}`)
-}
-calc(57);
+let x = prompt();
+x = x.toString();
+console.log(x.split(""));
+alert(x.split(""));
 
 
 
