a2ef353d5c7649bee885ec8942af9b1a0ca45439
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-03-31 00:26:28 +0400

    tv03.js is done

--- JsDataStructures/script/tv03.js
+++ JsDataStructures/script/tv03.js
@@ -25,3 +25,9 @@ let students = [{
 //expected output
 // Ad: Eli,Soyad:Piriyev,Yas:23
 // Ad: Ehmed,Soyad:Memmedov,Yas:34
+
+console.log(students[0]["ad"], students[0]["soyad"], students[0]["yas"]);
+console.log(students[1]["ad"], students[1]["soyad"], students[1]["yas"]);
+console.log(students[2]["ad"], students[2]["soyad"], students[2]["yas"]);
+console.log(students[3]["ad"], students[3]["soyad"], students[3]["yas"]);
+console.log(students[4]["ad"], students[4]["soyad"], students[4]["yas"]);
