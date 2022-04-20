0b0d0b00815fc5444cd8bf185486f3d769995043
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-04-20 20:18:08 +0400

    created js files

---
+++ DOMCircles/script/StrongPassword.js
@@ -0,0 +1,5 @@
+// Its length is at least 6
+// contains at least one digit
+// It contains at least one lowercase English character.
+// It contains at least one uppercase English character.
+// It contains at least one special character. The special characters are: !@#$%^&*()-+
